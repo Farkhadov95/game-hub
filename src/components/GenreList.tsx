d930abd02fe0eb75f478d86cb589f5ac3bc2e935
Farkhadov95 <farkhadov950405@gmail.com>
2023-05-29 13:53:46 +0100

    updated usePlatform and useGenres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -27,7 +27,7 @@ const GenreList = ({ onGenreClick, selectedGenre }: GenreListProps) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
